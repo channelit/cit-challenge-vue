c2b4b39048341826106d650aa0b800b1e391230f
Mohamed Gaily <mgaily@harmonia.com>
2018-09-06 21:13:54 -0400

    preliminary wireframe

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <main-container></main-container>
+    </div>
+</template>
+
+<script>
+import MainContainer from './components/MainContainer.vue';
+
+export default {
+    components: {
+        MainContainer
+    }
+}
+</script>
+
+<style>
+</style>
